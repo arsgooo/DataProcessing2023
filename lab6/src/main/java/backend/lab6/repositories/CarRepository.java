9fcc917362b39e7f9ac98da501aacd806612bcb5
Arsenii Dobrovolskyi <cleverboyars@gmail.com>
2023-06-08 13:00:30 +0300

    Lab7 added

--- lab6/src/main/java/backend/lab6/repositories/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package backend.lab6.repositories;
-
-import backend.lab6.entities.CarEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<CarEntity, Long> {
-}
--- lab6f/src/app/interfaces/car.ts
+++
@@ -1,6 +0,0 @@
-export interface Car {
-  id: number;
-  img: string;
-  name: string;
-  price: number;
-}
